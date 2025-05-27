62c5865d31df0ba5b60d94a3e7861b207cc46646
heikkuj <heikkuamo@gmail.com>
2025-05-27 19:11:57 +0200

    added signup, login, and authentication through supabase

---
+++ app/auth/callback.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { supabase } from '../../lib/supabase';
+
+export default function AuthCallback() {
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleAuthCallback = async () => {
+            const { data, error } = await supabase.auth.getSession();
+            if (data.session) {
+                router.push('/');
+            }
+        }
+
+        handleAuthCallback();
+    }, [router]);
+
+    return <div>Processing authentication...</div>
+}
