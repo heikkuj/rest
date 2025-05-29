ba9c7562affb710f48aa66e8c1f224c00cc39355
heikkuj <heikkuamo@gmail.com>
2025-05-29 19:02:42 +0200

    added header

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='flex flex-col w-full my-5'>
+        <h1 className='font-coiny text-4xl uppercase text-center'>Rest</h1>
+    </div>
+  )
+}
--- components/Login.js
+++ components/Login.js
@@ -5,12 +5,8 @@ import { useAuth } from '../context/AuthContext';
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
 } from "@/components/ui/card"
-import { Car } from 'lucide-react';
+import Header from './Header';
 
 export default function Login() {
     const [email, setEmail] = useState('');
@@ -39,9 +35,10 @@ export default function Login() {
         }
 
         return (
-
-            <div className='flex flex-col m-[20px] p-[20px]'>
-                <Card className='h-[35vh] bg-orange-400'>
+            <div>
+                <Header />
+                <div className='flex flex-col m-[20px] p-[20px]'>
+                <Card className='h-auto bg-orange-400'>
                     <CardContent>
                         <form onSubmit={handleSubmit} className='flex flex-col w-full items-center py-2'>
                         <input
@@ -81,46 +78,6 @@ export default function Login() {
                     </CardContent>
                 </Card>
             </div>
-
-            // <div className='flex flex-col m-[20px] p-[20px] bg-orange-400 rounded-2xl'>
-
-            //     <div className='flex flex-col items-center'>
-            //         <form onSubmit={handleSubmit} className='flex flex-col w-full items-center'>
-            //             <input
-            //                 type='email'
-            //                 value={email}
-            //                 onChange={(e) => setEmail(e.target.value)}
-            //                 placeholder='Epost'
-            //                 required
-            //                 className='p-2 my-1 w-full rounded-md'
-            //                 />
-
-            //             <input 
-            //                 type='password'
-            //                 value={password}
-            //                 onChange={(e) => setPassword(e.target.value)}
-            //                 placeholder='Passord'
-            //                 required
-            //                 className='p-2 my-1 w-full rounded-md'
-            //                 />
-
-            //             <button type='submit' 
-            //             className='font-bold bg-white w-fit rounded-2xl px-3 py-2 m-3'>
-            //                 {isSignUp ? 'Opprett bruker' : 'Logg inn'}
-            //             </button>
-            //         </form>
-            //         <div>
-            //             <button onClick={() => setIsSignUp(!isSignUp)}>
-            //                 {isSignUp ? 'Har du allerede en bruker? Logg på' : 'Ny bruker?'}
-            //             </button>
-
-            //             {message && (
-            //                 <div>
-            //                     {message}
-            //                 </div>
-            //             )}
-            //         </div>
-            //     </div>
-            // </div>
+        </div>
         )
     }
