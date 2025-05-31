da2aea4c4becd9a788f4dd6fc898c9a58eee495c
heikkuj <heikkuamo@gmail.com>
2025-05-31 21:50:11 +0200

    added show/hide toggle to user id

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -2,11 +2,17 @@
 
 import { useAuth } from "../context/AuthContext";
 import Header from "./Header";
-import { Card, CardContent } from './ui/card';
+import { Card, CardContent, CardHeader } from './ui/card';
 import Greeting from './Greeting';
+import { useEffect, useState } from "react";
 
 export default function Dashboard() {
+    const [showID, setShowID] = useState(false);
     const { user, signOut } = useAuth();
+
+    const toggleIdVisibility = () => {
+        setShowID(!showID);
+    };
 
     const handleSignOut = async () => {
         await signOut();
@@ -17,14 +23,28 @@ export default function Dashboard() {
             <Header />
             <div className='flex flex-col m-[20px] p-[20px]'>
                 <Card className='h-auto mb-10 bg-orange-400'>
+                    <CardHeader className="font-coiny text-xl">
+                        <Greeting />
+                    </CardHeader>
 
                     <CardContent>
-                        <Greeting />
-
-                        <div className='flex flex-col text-center justify-center'>
-                            
-
+                        <div className="font-coiny">
+                            <h1>Min profil</h1>
                         </div>
+
+                        {/* Profile information */}
+                        <div className='flex flex-col gap-1'>
+                            <p>E-post: {user.email}</p>
+
+                            <p>Bruker-ID: <button className="underline" onClick={toggleIdVisibility}>
+                                {showID ? 'Skjul' : 'Vis'}</button>
+                            {showID && (
+                                <span> {user?.id} </span>
+                            )}
+                            </p>
+                        </div>
+                        <button className="mb-3 mt-5 underline"
+                        onClick={handleSignOut}>Logg ut</button>
                     </CardContent>
                 </Card>
 
